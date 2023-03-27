198cd01151a308793f31fc7c3880010f26d7d06a
Igor Silva <igorbr2018@gmail.com>
2023-03-27 20:03:36 -0300

    create services

---
+++ src/services/FormAnswerService/CreateFormAnswerService/index.ts
@@ -0,0 +1,30 @@
+import { FormAnswer } from "@prisma/client"
+import { prisma } from "@prisma"
+
+interface ICreateFormAnswerServiceDTO {
+  data: {
+    name: string,
+    email: string,
+    cpf: string,
+    phone: string
+  }
+}
+
+interface ICreateFormAnswerService {
+  execute({ data }: ICreateFormAnswerServiceDTO): Promise<FormAnswer>
+}
+
+export class CreateFormAnswerService implements ICreateFormAnswerService {
+  async execute({ data }: ICreateFormAnswerServiceDTO): Promise<FormAnswer> {
+    try {
+      const formAnswer = await prisma.formAnswer.create({
+        data
+      })
+
+      return formAnswer
+    } catch (error) {
+      throw error;
+    }
+
+  }
+}
---
+++ src/services/FormAnswerService/GetFormAnswerService/index.ts
@@ -0,0 +1,43 @@
+import { FormAnswer } from "@prisma/client"
+import { prisma } from "@prisma"
+
+interface IGetFormAnswerServiceDTO {
+  filters?: {
+    startDate?: string,
+    endDate?: string
+  },
+  count?: number
+}
+
+interface IGetFormAnswerService {
+  execute(select?: IGetFormAnswerServiceDTO): Promise<FormAnswer[]>
+}
+
+export class GetFormAnswerService implements  IGetFormAnswerService{
+  async execute(select?: IGetFormAnswerServiceDTO): Promise<FormAnswer[]> {
+    try {
+      if (select?.filters) {
+        const formsAnswers =  await prisma.formAnswer.findMany({
+          where: {
+            createdAt: {
+              gte: select.filters.startDate,
+              lt: select.filters.endDate,
+            },
+          },
+          take: select.count || 10
+        })
+
+        return formsAnswers
+      }
+
+      const formsAnswers = await prisma.formAnswer.findMany({
+        take: select?.count || 10
+      })
+
+      return formsAnswers
+    } catch (error) {
+      throw error;
+    }
+
+  }
+}
