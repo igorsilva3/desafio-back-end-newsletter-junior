86c6421cf0976e2a73dbbb3d8b690952fe7b2eaa
Igor Silva <igorbr2018@gmail.com>
2023-03-27 21:10:09 -0300

    Updated routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,16 @@
-import { Router, Response } from 'express'
+import { Router } from 'express'
 
 import { formAnswerRoutes } from '@routes/formAnswerRoutes'
+import { docsRouter } from '@routes/docsRouter'
 
 const routes = Router()
 
 routes.use('/answers', formAnswerRoutes)
+routes.use('/api-docs', docsRouter)
+
+routes.get('/', (req, res) => {
+  res.redirect('/api-docs')
+})
+
 
 export { routes }
